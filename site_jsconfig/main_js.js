5f153bc39239ea567eaf6eba92a2643aa82ec3f6
juacab <ing_arismendy@hotmail.com>
2017-06-22 23:50:59 -0500

    Index sample - funcional
    Algunos errores en responsive pero index funcional, mejorar hover para dispositivos moviles

--- site_jsconfig/main_js.js
+++ site_jsconfig/main_js.js
@@ -1,13 +1,13 @@
 $(window).on('load', function() { // makes sure the whole site is loaded 
-  $('#status').delay(1500).fadeOut('slow'); // will first fade out the loading animation 
+  /*$('#status').delay(1500).fadeOut('slow'); // will first fade out the loading animation 
   $('#preloader').delay(2000).slideToggle('slow'); // will fade out the white DIV that covers the website. 
-  $('body').delay(350).css({'overflow':'visible'});
+  $('body').delay(350).css({'overflow':'visible'});*/
 
   
   
 
 
   $("#front-slide").css("height",$(window).height()-200);
-  $(".img_bg").css("height",$(window).height());
-  $(".img_sm").css("width",$(window).width());
+  $(".img_bg").css("height",$(window).height()-200);
+  $(".img_sm").css("width",$(window).width())-200;
 })
